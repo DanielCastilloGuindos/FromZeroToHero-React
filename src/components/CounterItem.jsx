50300a7e2252b22b4b046c82b97a9e03062fd48c
Daniel Castillo Guindos <danielcastilloguindos@gmail.com>
2023-11-25 02:44:04 +0100

    Añadimos funcionalidades para el contador

--- src/components/CounterItem.jsx
+++ src/components/CounterItem.jsx
@@ -1,37 +1,53 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export const CounterItem = ({ title, subtitle, initState = 0, lastState = 1, timer = 2000 }) => {
-	// const [ counter, setCounter ] = useState(initState);
-	// const fps = (timer) / (lastState - initState);
-	// const increment = 50;
-	// const interval = useRef();
+  const [currentCount, setCurrentCount] = useState(initState);
+  const containerRef = useRef(null);
 
-	// const handleCounter = () => {
-	// 	clearInterval(interval.current)
-	// 	interval.current = setInterval(() => {
-	// 		if (counter < lastState)
-	// 			setCounter(current => {
-	// 				const nextValue = current + increment;
-	// 				return nextValue > lastState ? lastState : nextValue;
-	// 			});
-	// 	}, fps)
-	// }
+  useEffect(() => {
+    if (lastState <= initState) {
+      console.error("Error: lastState debe ser mayor que initState");
+      return;
+    }
 
-	// useEffect(() => {
+		// Incremento más frecuente
+    const steps = Math.ceil((lastState - initState) / (timer / 100));
 
-	// 	if (counter < lastState)
-	// 		handleCounter();
+    const handleIntersection = (entries) => {
+      if (entries[0].isIntersecting) {
+        const intervalId = setInterval(() => {
+          setCurrentCount((prevCount) => {
+            const nextCount = prevCount + steps;
+            return nextCount < lastState ? nextCount : lastState;
+          });
+        }, timer / 100);  // Dividido por 100 para obtener la décima parte del tiempo
 
-	// 	return () => {
-	// 		clearInterval(interval.current)
-	// 	}
-	// }, [ lastState ]);
+        return () => clearInterval(intervalId);
+      }
+    };
 
-	return (
-		<figure className='flex flex-col gap-4 justify-center items-center'>
-			<h5 className='text-[1.65rem] leading-9 tracking-wider'>{ title }</h5>
-			<span className='text-[0.95rem] font-[Helvetica] tracking-wider'>{ subtitle }</span>
-			<p className='text-5xl font-[Slim-Joe]'>{ lastState }</p>
-		</figure>
-	);
-}
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1, // Cuando al menos el 10% del componente es visible
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [lastState, timer]);
+
+  return (
+    <figure ref={containerRef} className='flex flex-col gap-4 justify-center items-center'>
+      <h5 className='text-[1.65rem] leading-9 tracking-wider'>{title}</h5>
+      <span className='text-[0.95rem] font-[Helvetica] tracking-wider'>{subtitle}</span>
+      <p className='text-5xl font-[Slim-Joe]'>{currentCount}</p>
+    </figure>
+  );
+};
